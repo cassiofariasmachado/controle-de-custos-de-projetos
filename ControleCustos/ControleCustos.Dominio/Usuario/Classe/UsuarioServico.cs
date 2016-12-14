9f8cfba1164d86b43a52f2fd017004bf4b809e81
Gabriel <rosa.gabriel@cwi.com.br>
2016-12-14 16:58:17 -0200

    refactor(Usuario): retirar using não usado

--- ControleCustos/ControleCustos.Dominio/Usuario/Classe/UsuarioServico.cs
+++ ControleCustos/ControleCustos.Dominio/Usuario/Classe/UsuarioServico.cs
@@ -1,10 +1,5 @@
 ﻿using ControleCustos.Dominio.Criptografia.Interface;
 using ControleCustos.Dominio.Usuario.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControleCustos.Dominio.Usuario.Classe
 {
