b7121dcbd81037d9c47cadabcb8c25f9dd5a0dab
Gabriel <rosa.gabriel@cwi.com.br>
2016-12-14 18:37:18 -0200

    feat(Repositorio): implementar a classe DatabaseContext

---
+++ ControleCustos/ControleCustos.Repositorio/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using ControleCustos.Dominio.UsuarioDominio.Classe;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ControleCustos.Repositorio
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("ControleCustos")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
