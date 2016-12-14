0d1088e3deae789cbb1b169df0075dfd78e9de4a
Gabriel <rosa.gabriel@cwi.com.br>
2016-12-14 18:36:52 -0200

    feat(Usuario): implementar UsuarioRepositorio

---
+++ ControleCustos/ControleCustos.Repositorio/UsuarioRepositorio/UsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using ControleCustos.Dominio.UsuarioDominio.Interface;
+using System.Linq;
+using ControleCustos.Dominio.UsuarioDominio.Classe;
+
+namespace ControleCustos.Repositorio.UsuarioRepositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Usuario BuscarPorEmail(string email)
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Usuario.FirstOrDefault(u => u.Email.Equals(email));
+            }
+        }
+    }
+}
