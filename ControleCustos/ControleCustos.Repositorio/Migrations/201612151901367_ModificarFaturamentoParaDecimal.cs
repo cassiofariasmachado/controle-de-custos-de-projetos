3e6557f8dab9c7bdd71b78f3163690a55c69b0cf
Mateus M. R <matmr.prog@gmail.com>
2016-12-15 17:02:37 -0200

    fix(Projeto): corrigir tipo do atributo faturamento para decimal

---
+++ ControleCustos/ControleCustos.Repositorio/Migrations/201612151901367_ModificarFaturamentoParaDecimal.cs
@@ -0,0 +1,16 @@
+namespace ControleCustos.Repositorio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ModificarFaturamentoParaDecimal : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
