288aaa61a2130d7832e6ad09f684a955c65e4c25
Unknown <gabrielfr97@gmail.com>
2016-12-14 22:50:10 -0200

    feat(Login): implementar a LoginController

---
+++ ControleCustos/ControleCustos/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleCustos.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logar(string usuario, string senha)
+        {
+            Usuario usuarioAutenticado = ServicoDeUsuario.BuscarUsuarioAutenticado(
+                    usuario, senha);
+
+            if (usuarioAutenticado != null)
+            {
+                ServicoDeAutenticacao.Autenticar(new UsuarioLogadoModel(
+                    usuarioAutenticado.Nome, usuarioAutenticado.Permissoes));
+                return RedirectToAction("Secreta");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
