5e2107b686dd88eb5b5ed84607f670c74911738b
Mateus M. R <matmr.prog@gmail.com>
2016-12-15 16:23:49 -0200

    refactor(SituacaoProjeto): refatorar enum para adicionar descrição

--- ControleCustos/ControleCustos.Dominio/Projeto/Enum/SituacaoProjeto.cs
+++ ControleCustos/ControleCustos.Dominio/Projeto/Enum/SituacaoProjeto.cs
@@ -1,10 +1,20 @@
-﻿namespace ControleCustos.Dominio.Projeto.Enum
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleCustos.Dominio.Projeto.Enum
 {
     public enum SituacaoProjeto
     {
+        [Display(Name = "Novo")]
         Novo,
+
+        [Display(Name = "Em andamento")]
         EmAndamento,
+
+        [Display(Name = "Cancelado")]
         Cancelado,
+
+        [Display(Name = "Concluído")]
         Concluido
     }
 }
