95efe52c4b23ed9ad2b157ad7fe57c8caf164ca2
Unknown <gabrielfr97@gmail.com>
2016-12-14 22:51:35 -0200

    feat(Segurança): implementar ServicoDeAutenticação

---
+++ ControleCustos/ControleCustos/Servicos/ServicoDeAutenticacao.cs
@@ -0,0 +1,25 @@
+﻿using ControleCustos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleCustos.Servicos
+{
+    public class ServicoDeAutenticacao
+    {
+        private const string USUARIO_LOGADO_CHAVE = "USUARIO_LOGADO_CHAVE";
+        public static void Autenticar(UsuarioModel model)
+        {
+            HttpContext.Current.Session[USUARIO_LOGADO_CHAVE] = model;
+        }
+
+        public static UsuarioModel UsuarioLogado
+        {
+            get
+            {
+                return (UsuarioModel)HttpContext.Current.Session[USUARIO_LOGADO_CHAVE];
+            }
+        }
+    }
+}
