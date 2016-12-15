12a7a7fa380052b5d1b240de620e8076360534e5
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-12-15 16:40:27 -0200

    feat(ProjetoRepositorio): adicionar repositorio com métodos Buscar(), Inserir() e Atualizar().

---
+++ ControleCustos/ControleCustos.Repositorio/ProjetoRepositorio/ProjetoRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using ControleCustos.Dominio;
+using ControleCustos.Dominio.Interface;
+
+namespace ControleCustos.Repositorio.ProjetoRepositorio
+{
+    public class ProjetoRepositorio : IProjetoRepositorio
+    {
+        public Projeto Buscar(int id)
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Projeto.Find(id);
+            }
+        }
+
+        public void Inserir(Projeto projeto)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Entry<Projeto>(projeto).State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Projeto projeto)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Entry<Projeto>(projeto).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
