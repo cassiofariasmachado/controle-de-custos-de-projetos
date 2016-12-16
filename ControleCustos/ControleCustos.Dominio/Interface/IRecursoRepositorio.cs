44d1eb4efa050c7fe6400c7af0490b0f18ed1565
Unknown <gabrielfr97@gmail.com>
2016-12-16 10:48:48 -0200

    feat(Recurso): adicionar novo metodo(BuscaPaginada) a interface

--- ControleCustos/ControleCustos.Dominio/Interface/IRecursoRepositorio.cs
+++ ControleCustos/ControleCustos.Dominio/Interface/IRecursoRepositorio.cs
@@ -1,9 +1,12 @@
-﻿namespace ControleCustos.Dominio.Interface
+﻿using System.Collections.Generic;
+
+namespace ControleCustos.Dominio.Interface
 {
     public interface IRecursoRepositorio
     {
         Recurso Buscar(int id);
         void Inserir(Recurso recurso);
         void Atualizar(Recurso recurso);
+        IList<Recurso> BuscaPaginada(Recurso tipo, int pagina, int quantidade);
     }
 }
