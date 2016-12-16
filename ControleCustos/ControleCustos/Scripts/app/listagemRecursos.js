8ec7c5121f28139832b129088f098b8807aab0f7
Unknown <gabrielfr97@gmail.com>
2016-12-16 15:37:59 -0200

    fix(ListagemRecursos): consertar url

---
+++ ControleCustos/ControleCustos/Scripts/app/listagemRecursos.js
@@ -0,0 +1,50 @@
+﻿let listagemRecursos = {};
+
+listagemRecursos.paginaAtual = 0;
+listagemRecursos.lista = $('recurso-lista');
+
+listagemRecursos.carregarListaDeRecursos = function () {
+    $.ajax({
+        url: 'CarregarListaDeRecursos',
+        type: 'GET',
+        data: {
+            pagina: listagemRecursos.paginaAtual
+        }
+    })
+    .then(function (resultado) {
+        lista.html(resultado);
+        listagemRecursos.atualizarBotoesDeNavegacao();
+    })
+}
+
+listagemRecursos.voltarPagina = function () {
+    if (listagemRecursos.paginaAtual > 0) {
+        listagemRecursos.paginaAtual--;
+        listagemRecursos.carregarListaDeRecursos();
+    }
+}
+
+listagemRecursos.avancarPagina = function () {
+    listagemRecursos.paginaAtual++;
+    listagemRecursos.carregarListaDeRecursos();
+}
+
+listagemRecursos.atualizarBotoesDeNavegacao = function () {
+    listagemRecursos.$btnVoltarPagina.attr('disabled', listagemRecursos.paginaAtual === 0);
+
+    listagemRecursos.$btnAvancarPagina.attr('disabled', ultimaPagina);
+}
+
+listagemRecursos.configurarBotoesDeNavegacao = function () {
+    listagemRecursos.$btnVoltarPagina.click(listagemRecursos.voltarPagina);
+    listagemRecursos.$btnAvancarPagina.click(listagemRecursos.avancarPagina);
+}
+
+listagemRecursos.iniciar = function () {
+    listagemRecursos.$btnVoltarPagina = $("#btn-voltar-pagina");
+    listagemRecursos.$btnAvancarPagina = $("#btn-avancar-pagina");
+
+    listagemRecursos.configurarBotoesDeNavegacao();
+
+    listagemRecursos.carregarListaDeRecursos();
+}
