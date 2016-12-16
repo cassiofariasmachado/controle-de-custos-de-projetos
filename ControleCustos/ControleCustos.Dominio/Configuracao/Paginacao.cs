c5d2204c5e3fb0755a57a728d973a36b8730141a
Unknown <gabrielfr97@gmail.com>
2016-12-16 10:46:24 -0200

    feat(Configuracao): implementar classe de Paginacao

---
+++ ControleCustos/ControleCustos.Dominio/Configuracao/Paginacao.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleCustos.Dominio.Configuracao
+{
+    public class Paginacao
+    {
+        public int PaginaDesejada { get; set; }
+        public int QuantidadeDeItensPorPagina { get; set; }
+    }
+}
