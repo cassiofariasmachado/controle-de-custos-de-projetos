bdf5b785a4ee91a33a0e20c5e60fc01b3bbde97a
Unknown <gabrielfr97@gmail.com>
2016-12-16 10:57:52 -0200

    feat(Configuracao): implementar a classe de servico de configuracao

---
+++ ControleCustos/ControleCustos/Servicos/ServicoDeConfiguracao.cs
@@ -0,0 +1,18 @@
+﻿using ControleCustos.Dominio.Interface;
+using System;
+using System.Configuration;
+
+namespace ControleCustos.Servicos
+{
+    public class ServicoDeConfiguracao : IServicoDeConfiguracao
+    {
+        public int QuantidadeDeItensPorPagina
+        {
+            get
+            {
+                int quantidadeDeRecursosPorPagina = Convert.ToInt32(ConfigurationManager.AppSettings["QuantidadeDeRecursosPorPagina"]);
+                return quantidadeDeRecursosPorPagina;
+            }
+        }
+    }
+}
