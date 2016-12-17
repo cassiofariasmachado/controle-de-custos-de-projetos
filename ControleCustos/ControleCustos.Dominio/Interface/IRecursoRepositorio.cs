de2f442ee9c1a11ed583948f346341d39f7fe79b
Unknown <gabrielfr97@gmail.com>
2016-12-17 15:57:11 -0200

    feat(Recusos): adicionar novos metodos

--- ControleCustos/ControleCustos.Dominio/Interface/IRecursoRepositorio.cs
+++ ControleCustos/ControleCustos.Dominio/Interface/IRecursoRepositorio.cs
@@ -7,6 +7,8 @@ namespace ControleCustos.Dominio.Interface
         Recurso Buscar(int id);
         void Inserir(Recurso recurso);
         void Atualizar(Recurso recurso);
-        IList<Recurso> BuscaPaginada(Recurso tipo, int pagina, int quantidade);
+        IList<Recurso> BuscaPaginadaRecursoCompartilhados(int pagina, int quantidade);
+        IList<Recurso> BuscaPaginadaPatrimonios(int pagina, int quantidade);
+        IList<Recurso> BuscaPaginadaServicos(int pagina, int quantidade);
     }
 }
