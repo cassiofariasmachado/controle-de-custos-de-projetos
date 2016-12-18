fb835bcddc4aabc0f66f2313aaf022f873c0fd45
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-12-17 23:48:22 -0200

    feat(CalculoServico): adicionar serviço para calculos utilizados nos relatórios.

---
+++ ControleCustos/ControleCustos.Dominio/CalculoServico.cs
@@ -0,0 +1,69 @@
+﻿using ControleCustos.Dominio.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleCustos.Dominio
+{
+    public class CalculoServico
+    {
+        private static int QUANTIDADE_DIAS_DO_MES = 30;
+
+        IControleRecursoRepositorio controleRecursoRepositorio;
+
+        public CalculoServico(IControleRecursoRepositorio controleRecursoRepositorio)
+        {
+            this.controleRecursoRepositorio = controleRecursoRepositorio;
+        }
+
+        public decimal CalcularCustoTotalAte(Projeto projeto, DateTime data)
+        {
+            decimal custoTotal = 0;
+            IList<ControleRecurso> listaControleRecurso = controleRecursoRepositorio.Listar(projeto);
+            foreach (var controleRecurso in listaControleRecurso)
+            {
+                // Verifica se recurso está nesse período
+                DateTime dataFim = data < controleRecurso.DataFim ? data : controleRecurso.DataFim;
+                if (data < controleRecurso.DataInicio) { continue; }
+                custoTotal += this.CalcularCustoTotalDoRecursoEntre(controleRecurso.Recurso, controleRecurso.DataInicio, dataFim);
+            }
+            return custoTotal;
+        }
+
+        public decimal CalcularCustoPercentual(Projeto projeto, DateTime data)
+        {
+            // custoPercentual = custoTotal * 100 / FaturamentoPrevisto
+            return this.CalcularCustoTotalAte(projeto, data) * 100 / projeto.FaturamentoPrevisto;
+        }
+
+        public decimal CalcularCustoMensal(Projeto projeto, int mes, int ano)
+        {
+            decimal custoTotal = 0;
+
+            DateTime primeiroDiaDoMes = new DateTime(ano, mes, 1);
+            DateTime ultimoDiaDoMes = primeiroDiaDoMes.AddMonths(1).AddDays(-1);
+            IList<ControleRecurso> listaControleRecurso = controleRecursoRepositorio.Listar(projeto, primeiroDiaDoMes, ultimoDiaDoMes);
+
+            foreach (var controleRecurso in listaControleRecurso)
+            {
+                // Verifica se recurso não acaba na metade do mês
+                DateTime dataInicio = controleRecurso.DataInicio > primeiroDiaDoMes ? controleRecurso.DataInicio : primeiroDiaDoMes;
+                DateTime dataFim = controleRecurso.DataFim < ultimoDiaDoMes ? controleRecurso.DataFim : ultimoDiaDoMes;
+                custoTotal += this.CalcularCustoTotalDoRecursoEntre(controleRecurso.Recurso, controleRecurso.DataInicio, dataFim);
+            }
+
+            return custoTotal;
+        }
+
+        public decimal CalcularCustoTotalDoRecursoEntre(Recurso recurso, DateTime dataInicio, DateTime dataFim)
+        {
+            decimal custoPorDia = recurso.ValorMensal / QUANTIDADE_DIAS_DO_MES;
+            var diferencaDatas = dataFim - dataInicio;
+            decimal quantidadeDias = (decimal)diferencaDatas.TotalDays;
+            decimal custoTotalDoRecurso = custoPorDia * quantidadeDias;
+            return custoTotalDoRecurso;
+        }
+    }
+}
---
+++ ControleCustos/ControleCustos.Tests/Dominio/CalculoServicoTest.cs
@@ -0,0 +1,110 @@
+﻿using ControleCustos.Dominio;
+using ControleCustos.Dominio.Enum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ControleCustos.Tests.Dominio
+{
+    [TestClass]
+    public class CalculoServicoTest
+    {
+        ControleRecursoRepositorioMock controleRecursoRepositorio = new ControleRecursoRepositorioMock();
+
+        [TestMethod]
+        public void CalcularCustoTotalDeveRetornarOCustoTotalDeProjetoComApenasUmRecurso()
+        {
+            Projeto projeto = new Projeto(1, "Coca-cola", new Usuario(), "Coca-cola", "C#", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoTotal = calculoServico.CalcularCustoTotalAte(projeto, new DateTime(2016, 12, 6));
+
+            Assert.AreEqual(73.3D, (double)custoTotal, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoTotalDeveRetornarOCustoTotalDeProjetoComDoisRecursos()
+        {
+            Projeto projeto = new Projeto(2, "Sicredi", new Usuario(), "Sicredi", "Java", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoTotal = calculoServico.CalcularCustoTotalAte(projeto, new DateTime(2016, 12, 6));
+
+            Assert.AreEqual(316.6D, (double)custoTotal, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoTotalNaoDeveConsiderarRecursosForaDoPeriodo()
+        {
+            Projeto projeto = new Projeto(2, "Sicredi", new Usuario(), "Sicredi", "Java", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoTotal = calculoServico.CalcularCustoTotalAte(projeto, new DateTime(2016, 11, 2));
+
+            Assert.AreEqual(33.3D, (double)custoTotal, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoMensalDeveRetornarCustoTotalDoMesInformado()
+        {
+            Projeto projeto = new Projeto(2, "Sicredi", new Usuario(), "Sicredi", "Java", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoTotal = calculoServico.CalcularCustoMensal(projeto, 11, 2016);
+
+            Assert.AreEqual(300D, (double)custoTotal, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoMensalDeveRetornarCustoTotalDoMesInformadoDesconsiderandoRecursosForaDoPeriodo()
+        {
+            Projeto projeto = new Projeto(2, "Sicredi", new Usuario(), "Sicredi", "Java", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoTotal = calculoServico.CalcularCustoMensal(projeto, 11, 2016);
+
+            Assert.AreEqual(300D, (double)custoTotal, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoPercentualDeveRetornarPercentualDeProjetoComApenasUmRecurso()
+        {
+            Projeto projeto = new Projeto(1, "Coca-cola", new Usuario(), "Coca-cola", "C#", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoPercentual = calculoServico.CalcularCustoPercentual(projeto, new DateTime(2016, 12, 6));
+
+            Assert.AreEqual(7.33D, (double)custoPercentual, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoPercentualDeveRetornarPercentualDeProjetoComDoisRecursos()
+        {
+            Projeto projeto = new Projeto(2, "Sicredi", new Usuario(), "Sicredi", "Java", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoPercentual = calculoServico.CalcularCustoPercentual(projeto, new DateTime(2016, 12, 6));
+
+            Assert.AreEqual(31.66, (double)custoPercentual, 0.1D);
+        }
+
+        [TestMethod]
+        public void CalcularCustoPercentualNaoDeveConsiderarRecursosForaDoPeriodo()
+        {
+            Projeto projeto = new Projeto(2, "Sicredi", new Usuario(), "Sicredi", "Java", new DateTime(2016, 11, 1), new DateTime(2016, 12, 5), 1000M, 12, SituacaoProjeto.Novo);
+
+            CalculoServico calculoServico = new CalculoServico(controleRecursoRepositorio);
+
+            decimal custoPercentual = calculoServico.CalcularCustoPercentual(projeto, new DateTime(2016, 11, 2));
+
+            Assert.AreEqual(3.3, (double)custoPercentual, 0.1D);
+        }
+    }
+}
