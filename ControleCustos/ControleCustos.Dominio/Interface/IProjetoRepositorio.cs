4f8236ba9ce503d489d5d772f6280b0e2a8a7d7f
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-12-19 21:58:19 -0200

    fix(ProjetoRepositorio, IProjetoRepositorio, RelatorioController): substituir método ListarProjetosEmAndamento() por ListarProjetosAtivos().

--- ControleCustos/ControleCustos.Dominio/Interface/IProjetoRepositorio.cs
+++ ControleCustos/ControleCustos.Dominio/Interface/IProjetoRepositorio.cs
@@ -6,7 +6,7 @@ namespace ControleCustos.Dominio.Interface
     public interface IProjetoRepositorio
     {
         Projeto Buscar(int id);
-        IList<Projeto> ListarProjetosEmAndamento();
+        IList<Projeto> ListarProjetosAtivos();
         IList<Projeto> ListarProjetosEncerrados();
         void Inserir(Projeto projeto);
         void Atualizar(Projeto projeto);
