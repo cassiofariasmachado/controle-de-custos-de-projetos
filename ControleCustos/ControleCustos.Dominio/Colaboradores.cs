05a4eaf6ee7e16848f89f3a69b45df1965495248
Unknown <gabrielfr97@gmail.com>
2016-12-20 16:17:48 -0200

    feat(Colaboradores): implementar classe dominio

---
+++ ControleCustos/ControleCustos.Dominio/Colaboradores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ControleCustos.Dominio
+{
+    public class Colaboradores
+    {
+        public int Id { get; private set; }
+        public int QuantidadeDeColaboradores { get; private set; }
+        public DateTime Periodo { get; private set; }
+        public UnidadeTecnica UnidadeTecnica { get; private set; }
+        public Colaboradores(int id, int quantidadeDeColaboradores, DateTime periodo, UnidadeTecnica unidadeTecnica)
+        {
+            this.Id = id;
+            this.QuantidadeDeColaboradores = quantidadeDeColaboradores;
+            this.Periodo = periodo;
+            this.UnidadeTecnica = unidadeTecnica;
+        }
+    }
+}
