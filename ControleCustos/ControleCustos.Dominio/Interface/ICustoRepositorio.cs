26477fed335bf10d8f7574f46814950948af7442
Unknown <gabrielfr97@gmail.com>
2016-12-20 18:05:50 -0200

    feat(CustoRepositorio): adicionar nova query

--- ControleCustos/ControleCustos.Dominio/Interface/ICustoRepositorio.cs
+++ ControleCustos/ControleCustos.Dominio/Interface/ICustoRepositorio.cs
@@ -1,6 +1,9 @@
-﻿namespace ControleCustos.Dominio.Interface
+﻿using System;
+
+namespace ControleCustos.Dominio.Interface
 {
     public interface ICustoRepositorio
     {
+        decimal CalcularCustoFixo(UnidadeTecnica unidadeTecnica, DateTime periodo);
     }
 }
