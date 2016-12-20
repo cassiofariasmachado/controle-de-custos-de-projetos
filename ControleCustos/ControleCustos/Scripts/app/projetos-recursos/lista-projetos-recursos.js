63c3b4369dcdbea08fac2efa56aceb3daaca5d25
Alexander_Not <matmr.prog@gmail.com>
2016-12-20 10:32:59 -0200

    feat(Detalhe.cshtml): implementar base para página de detalhes do projeto

---
+++ ControleCustos/ControleCustos/Scripts/app/projetos-recursos/lista-projetos-recursos.js
@@ -0,0 +1,39 @@
+﻿listaProjetosRecursos = {};
+
+$idProjeto = $("id-projeto");
+
+$(() => {
+
+    listaProjetosRecursos.abrirSpinner();
+
+    $.get('/Projeto/CarregarListaDeRecursosDoProjeto', { idProjeto: $('#id-projeto').data("id-projeto") },
+        function (retorno) {
+            listaProjetosRecursos.fecharSpinner();
+            $("#projeto-recursos-lista").html(retorno);
+        });
+});
+
+$("#botao-busca").click(function (event) {
+    event.preventDefault();
+    let filtro = $('#caixa-busca').val();
+
+    listaProjetosRecursos.abrirSpinner();
+    $.ajax({
+        url: '/Projeto/CarregarListaDeRecursosDoProjeto',
+        data: { "filtro": filtro },
+        success: function (data) {
+            listaProjetosRecursos.fecharSpinner();
+            $("#projeto-recursos-lista").html(data);
+        }
+    });
+});
+
+listaProjetosRecursos.abrirSpinner = function () {
+    $('#projeto-recursos-lista').addClass('hide');
+    $('.spinner').removeClass('hide');
+}
+
+listaProjetosRecursos.fecharSpinner = function () {
+    $('.spinner').addClass('hide');
+    $('#projeto-recursos-lista').removeClass('hide');
+}
