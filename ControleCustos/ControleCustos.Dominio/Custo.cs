c47fda8420c8c5cab54570c1e047199676d366cc
Unknown <gabrielfr97@gmail.com>
2016-12-20 16:13:04 -0200

    feat(Custo): implementar classe no dominio

---
+++ ControleCustos/ControleCustos.Dominio/Custo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ControleCustos.Dominio
+{
+    public class Custo
+    {
+        public int Id { get; private set; }
+        public decimal EnergiaEletrica { get; private set; }
+        public decimal Agua { get; private set; }
+        public decimal MaterialExpediente { get; private set; }
+        public decimal MaterialEscritorio { get; private set; }
+        public decimal Condominio { get; private set; }
+        public decimal Aluguel { get; private set; }
+        public decimal Internet { get; private set; }
+        public UnidadeTecnica UnidadeTecnica { get; private set; }
+        public DateTime Periodo { get; private set; }
+
+        public Custo(int id, decimal energiaEletrica, decimal agua, decimal materialExpediente, decimal materialEscritorio, decimal condominio, decimal aluguel, decimal internet, UnidadeTecnica UnidadeTecnica, DateTime Periodo)
+        {
+            this.Id = id;
+            this.EnergiaEletrica = energiaEletrica;
+            this.Agua = agua;
+            this.MaterialExpediente = materialExpediente;
+            this.MaterialEscritorio = materialEscritorio;
+            this.Condominio = condominio;
+            this.Aluguel = aluguel;
+            this.Internet = internet;
+            this.UnidadeTecnica = UnidadeTecnica;
+            this.Periodo = Periodo;
+        }
+    }
+}
