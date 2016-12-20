c19bbec480dc81ade324f8b0238091413b1e6268
Alexander_Not <matmr.prog@gmail.com>
2016-12-20 00:16:28 -0200

    feat(ListaProjetos): adicionar loader

--- ControleCustos/ControleCustos/Scripts/app/projeto/lista.js
+++ ControleCustos/ControleCustos/Scripts/app/projeto/lista.js
@@ -1,9 +1,16 @@
-﻿$(() => {
+﻿lista = {}
+
+$(() => {
+
+    lista.abrirSpinner();
+
     $.ajax({
         url: '/Projeto/ListaProjetosFiltrada',
         type: 'GET'
     })
+
     .then(function (resultado) {
+        lista.fecharSpinner();
         $('#projetos-lista').html(resultado);
     })
     .fail(function (erro) {
@@ -15,11 +22,23 @@ $("#botao-busca").click(function (event) {
     event.preventDefault();
     let filtro = $('#caixa-busca').val();
 
+    lista.abrirSpinner();
     $.ajax({
         url: '/Projeto/ListaProjetosFiltrada',
         data: { "filtro": filtro },
         success: function (data) {
+            lista.fecharSpinner();
             $("#projetos-lista").html(data);
         }
     });
 });
+
+lista.abrirSpinner = function () {
+    $('#projetos-lista').addClass('hide');
+    $('.spinner').removeClass('hide');
+}
+
+lista.fecharSpinner = function () {
+    $('.spinner').addClass('hide');
+    $('#projetos-lista').removeClass('hide');
+}
